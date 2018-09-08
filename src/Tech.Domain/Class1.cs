4a15ba12caa54f7cd3bb3c43975fb1fd9feb057b
Jonás Martínez Ullán <jonasmu@hotmail.com>
2018-09-08 20:45:10 -0300

    Domain with .NET Framework 4 added.

--- src/Tech.Domain/Class1.cs
+++ src/Tech.Domain/Class1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Tech.Domain
 {
     public class Class1
     {
-        // Testing.
+        // .NET Framework 4 testing.
     }
 }
